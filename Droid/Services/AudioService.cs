c5a2739cdec358177c45575a56ec94e1f0a30d59
Alex Dunn <alexander.dunn@outlook.com>
2017-07-26 10:00:11 -0400

    Add android implementation and updated page

---
+++ Droid/Services/AudioService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Android.Content;
+using Android.Media;
+using AudioTest.Droid;
+using AudioTest.Services;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(IAudioService))]
+namespace AudioTest.Droid.Services
+{
+    public class AudioService : IAudioService
+    {
+        public AudioService()
+        {
+        }
+
+        public void PlaySoundThroughEarPiece()
+        {
+            var mediaPlayer = MediaPlayer.Create(Android.App.Application.Context, Resource.Raw.sample_sound);
+            mediaPlayer.SetAudioStreamType(Stream.VoiceCall);
+
+            mediaPlayer.Start();
+        }
+
+        public void PlaySoundThroughSpeaker()
+        {
+            var mediaPlayer = MediaPlayer.Create(Android.App.Application.Context, Resource.Raw.sample_sound);
+            mediaPlayer.SetAudioStreamType(Stream.Music);
+
+            mediaPlayer.Start();
+        }
+    }
+}
