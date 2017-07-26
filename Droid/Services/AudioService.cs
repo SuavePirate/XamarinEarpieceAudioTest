dbb433bdeba31000e4740acc911f807841d60f4a
Alex Dunn <alexander.dunn@outlook.com>
2017-07-26 12:15:56 -0400

    Fixed android earpiece logic

--- Droid/Services/AudioService.cs
+++ Droid/Services/AudioService.cs
@@ -17,16 +17,28 @@ namespace AudioTest.Droid.Services
 
         public void PlaySoundThroughEarPiece()
         {
-            var mediaPlayer = MediaPlayer.Create(Android.App.Application.Context, Resource.Raw.sample_sound);
-            mediaPlayer.SetAudioStreamType(Stream.VoiceCall);
+            var mediaPlayer = new MediaPlayer();
 
+            mediaPlayer.Reset();
+
+            var audioManager = (AudioManager)Android.App.Application.Context.GetSystemService(Context.AudioService);
+            mediaPlayer.SetAudioStreamType(Stream.VoiceCall);
+            audioManager.Mode = Mode.InCall;
+            audioManager.SpeakerphoneOn = false;
+            mediaPlayer.SetDataSource(Android.App.Application.Context, Android.Net.Uri.Parse("android.resource://com.suavepirate.audiotest/raw/sample_sound"));
+            mediaPlayer.Prepare();
             mediaPlayer.Start();
         }
 
         public void PlaySoundThroughSpeaker()
         {
             var mediaPlayer = MediaPlayer.Create(Android.App.Application.Context, Resource.Raw.sample_sound);
+
+
+            var audioManager = (AudioManager)Android.App.Application.Context.GetSystemService(Context.AudioService);
             mediaPlayer.SetAudioStreamType(Stream.Music);
+            audioManager.Mode = Mode.Normal;
+            audioManager.SpeakerphoneOn = true;
 
             mediaPlayer.Start();
         }
